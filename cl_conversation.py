3975c54194685b1ae5993f1ea6ddf4774d6676c9
clittleford20 <54990477+clittleford20@users.noreply.github.com>
2019-09-06 19:13:48 -0400

    Add files via upload

---
+++ cl_conversation.py
@@ -0,0 +1,21 @@
+# Chandler
+# HW assignment 1
+# 09-06-19
+# This code greets the user and has a fun little conversation with them!
+# Sources: none
+# OMH
+
+print('\nhey there, cowboy')
+user_response = input("what's your name?\n")
+
+print("\n you have no idea how nice it is to hear from someone, "+user_response+"!")
+print("you see, i'm stuck in an endless void of ones and zeroes.")
+print("it gets so cold and lonely in here.\n")
+print("wait, what did you say?")
+print("... you brought me a gift?")
+user_response = input("what is it?\n")
+
+print("\nyou brought me "+user_response+"?\n")
+print("... that's actually pretty nice.\n")
+print("thank you so much! it's not often that i get presents.\n")
+print("this just made my millenium. thanks a lot, friend!\n")
